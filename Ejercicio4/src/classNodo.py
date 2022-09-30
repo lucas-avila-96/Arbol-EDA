297e54eaf3dbee0df1c900226a8e91187b63186a
lucas_avila_96 <56192229+lucas-avila-96@users.noreply.github.com>
2022-09-30 17:37:28 -0300

    Add files via upload

--- Ejercicio4/src/classNodo.py
+++ Ejercicio4/src/classNodo.py
@@ -1,30 +1,40 @@
 
 class Nodo:
+    __char = None
+    __frec = None
     __izq = None
     __der = None
-    __clave = None
 
-    def __init__(self, clave):
-        self.__clave = clave
+    def __init__(self, char, frec):
+        self.__char = char
+        self.__frec = frec
         self.__der = None
         self.__izq = None
     
+    def __gt__(self, otro):
+        return self.__frec > otro.getFrecuencia()
+
     def getIzquierdo(self):
         return self.__izq
+    
+    def getFrecuencia(self):
+        return self.__frec
+
+    def setFrecuencia(self, frec):
+        self.__frec = frec
 
     def getDerecho(self):
         return self.__der
 
     def getDato(self):
-        return self.__clave
+        return self.__char
 
-    def setDato(self, clave):
-        self.__clave = clave
+    def setDato(self, char):
+        self.__char = char
 
-    def setDerecho(self, clave):
-        self.__der = clave
+    def setDerecho(self, char):
+        self.__der = char
 
-    def setIzquierdo(self, clave):
-        self.__izq = clave
-
-
+    def setIzquierdo(self, char):
+        self.__izq = char
+    
